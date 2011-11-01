31d991bb6d8d06d74a7f5f6bd34f5bbe7a6fba91
Michael Gruar <splime200@gmail.com>
2011-11-01 19:37:47 -0400

    messing w/ run.bat

--- run.bat
+++
@@ -1,2 +0,0 @@
-START "Panda Game" C:\Panda3D-Runnable\python\python.exe "C:\Users\lummia2\ProjectThree\tutorial8.py"
-PAUSE
---
+++ run_chap.bat
@@ -0,0 +1,2 @@
+START "Panda Game" C:\Panda3D-1.7.2\python\python.exe "C:\Users\gruarm\Game Projects\ProjectThree\tutorial8.py"
+PAUSE
